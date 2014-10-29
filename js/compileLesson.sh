c0bcce14cdd5516a54a60d53cca73b5908bb95a0
VizIT <akluge@vizitsolutions.com>
2014-10-29 00:34:55 -0500

    Include older components in lesson.min.js for backward compatability.

--- js/compileLesson.sh
+++ js/compileLesson.sh
@@ -8,7 +8,11 @@ CLOSURE_HOME=/opt/closure
 
 $JAVA -jar $CLOSURE_HOME/compiler.jar --js current_license.js \
 IdRegistry.js RangedSource.js LessonBuilder.js \
-  --js_output_file Lesson.min.js
+  MappingEventHandler.js DirectEventHandler.js gaussianSurfaceRChangedEventListiner.js \
+  lessonContentEventHandler.js  LessonContentEventSource.js lessonEventHandler.js \
+  ModelImageEventHandler.js  RadialDependencyAdaptor.js  DotRAdapter.js \
+  SurfaceExtensionEventListiner.js ToExpression.js XvsYController.js \
+  --js_output_file lesson.min.js
 
 #  MappingEventHandler.js DirectEventHandler.js Lesson.js gaussianSurfaceRChangedEventListiner.js \
 #  lessonContentEventHandler.js  LessonContentEventSource.js lessonEventHandler.js \
